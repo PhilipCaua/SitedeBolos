760859b7d2939a9667ef8c6edd2a8c0f5b199765
PhilipCaua <160869311+PhilipCaua@users.noreply.github.com>
2025-09-22 21:36:23 -0300

    site_bolos

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-secondary mt-4">
+
+<div class="container text-center py-2">
+    <p class="mb-0 small text-white">© 2024 Lalá Cake. Todos os direitos reservados.</p>
+</div>
+</footer>
--- index.php
+++ index.php
@@ -2,56 +2,13 @@
 <html lang="pt-BR">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include_once("./layout/head.php"); ?>
     <title>Lalá Cake</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="icon" href="./img/favicon.png">
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- font-awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="p-0">
     <!-- Aqui é o menu -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="./img/favicon.png" alt="Logo" width="40" class="d-inline-block align-text-top">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="bolos.php">Bolos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="onde-encontrar.php">Onde encontrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="historia.php">História</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contatos.php">Contato</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php include_once("./layout/nav.php"); ?>
 
     <main>
         <p>&nbsp;</p>
@@ -77,16 +34,11 @@
         <p>&nbsp;</p>
     </main>
 
-    <footer class="bg-secondary mt-4">
+    <?php
+    include_once("./layout/footer.php");
+    include_once("./layout/script-js.php");
+    ?>
 
-        <div class="container text-center py-2">
-            <p class="mb-0 small text-white">© 2024 Lalá Cake. Todos os direitos reservados.</p>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
