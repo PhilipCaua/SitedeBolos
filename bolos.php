fbfe0c54ea62bbeb85a5d8ff73a4132d28d97e58
PhilipCaua <160869311+PhilipCaua@users.noreply.github.com>
2025-09-22 21:42:58 -0300

    Update bolos.php

---
+++ bolos.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <?php include_once("./layout/head.php"); ?>
+    <title>Lalá Cake - Nossos bolos</title>
+</head>
+
+<body class="p-0">
+    <!-- Aqui é o menu -->
+    <?php include_once("./layout/nav.php"); ?>
+
+    <main>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+    </main>
+
+    <?php
+    include_once("./layout/footer.php");
+    include_once("./layout/script-js.php");
+    ?>
+
+</body>
+
+</html>
